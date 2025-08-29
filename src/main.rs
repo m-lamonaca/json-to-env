42f30cbcf74d2bc82cacaf4c2ceafe5e04358de0
Marcello Lamonaca <marcello@lamonaca.eu>
2025-08-29 17:17:17 +0200

    remove input/output options

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
 use std::{
     error::Error,
-    fs::File,
-    io::{BufRead, BufReader, BufWriter, Read, Write},
+    io::{Read, Write},
 };
 
 use clap::Parser;
@@ -11,25 +10,14 @@ use serde_json::Value;
 fn main() -> Result<(), Box<dyn Error>> {
     let args = Args::parse();
 
-    let mut reader: Box<dyn BufRead> = match args.input {
-        None => Box::new(std::io::stdin().lock()),
-        Some(ref filename) => {
-            let file = File::open(filename)
-                .inspect_err(|_| eprintln!("Error: Could not open file `{filename}`"))?;
-
-            Box::new(BufReader::new(file))
-        }
-    };
-
     let mut buffer = String::new();
-
-    let input = args.input.unwrap_or("STDIN".to_string());
-    reader
+    std::io::stdin()
+        .lock()
         .read_to_string(&mut buffer)
-        .inspect_err(|_| eprintln!("Error: Could not read `{input}`"))?;
+        .inspect_err(|_| eprintln!("Error: Could not read input"))?;
 
     let json: Value = serde_json::from_str(&buffer)
-        .inspect_err(|_| eprintln!("Error: `{input}` does not contain valid JSON"))?;
+        .inspect_err(|_| eprintln!("Error: input does not contain valid JSON"))?;
 
     let options = ParseOptions::new(
         args.key_separator,
@@ -46,20 +34,10 @@ fn main() -> Result<(), Box<dyn Error>> {
         .collect::<Vec<String>>()
         .join("\n");
 
-    let mut writer: Box<dyn Write> = match args.output {
-        None => Box::new(std::io::stdout().lock()),
-        Some(ref filename) => {
-            let file = File::create(filename)
-                .inspect_err(|_| eprintln!("Error: Could not open file `{filename}`"))?;
-
-            Box::new(BufWriter::new(file))
-        }
-    };
-
-    let output = args.output.unwrap_or("STDOUT".to_string());
-    writer
+    std::io::stdout()
+        .lock()
         .write_all(environ.as_bytes())
-        .inspect_err(|_| eprintln!("Error: Could not write to `{output}`"))?;
+        .inspect_err(|_| eprintln!("Error: Could not write to stdout"))?;
 
     Ok(())
 }
@@ -67,14 +45,6 @@ fn main() -> Result<(), Box<dyn Error>> {
 #[derive(Debug, Parser)]
 #[command(name = "json2env", version, about)]
 struct Args {
-    /// Input file, defaults to STDIN if not specified
-    #[arg(short, long, value_name = "FILE")]
-    input: Option<String>,
-
-    /// Output file, defaults to STDOUT if not specified
-    #[arg(short, long, value_name = "FILE")]
-    output: Option<String>,
-
     /// Separator for nested keys
     #[arg(short = 's', long, value_name = "STRING", default_value = "__")]
     key_separator: String,
